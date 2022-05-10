20208e970caded42f6c85ad36db136a7e367a62a
Nam bv <nambv@versehub.io>
2022-05-10 15:30:50 +0700

    update slide

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,32 +26,32 @@ const Home: NextPage = () => {
     clickable: true,
     renderBullet: function (index: any, className: any) {
       const idx = index + 1;
-      return '<span class="' + className + '">' + '<img src="/icon' + idx + '.png"/>' + "</span>";
+      return '<span class="' + className + '">' + '<img src="/images/icon' + idx + '.png"/>' + "</span>";
     },
   };
   const dataSlide = [
     {
-      link: "/walking.mp4",
+      link: "/videos/walking.mp4",
       type: typeSlide.video,
       ref: refSlide1
     },
     {
-      link: "/item4.mp4",
+      link: "/videos/item4.mp4",
       type: typeSlide.video,
       ref: refSlide2
     },
     {
-      link: "/cycle.mp4",
+      link: "/videos/cycle.mp4",
       type: typeSlide.video,
       ref: refSlide3
     },
     {
-      link: "/item2.mp4",
+      link: "/videos/item2.mp4",
       type: typeSlide.video,
       ref: refSlide4
     },
     {
-      link: "/item5.jpg",
+      link: "/images/item5.jpg",
       type: typeSlide.img,
       // ref: refSlide5
     }
@@ -67,7 +67,7 @@ const Home: NextPage = () => {
         <meta property="og:title" content="Befitter is a Web3 lifestyle app with Social-Fi and Game-Fi elements" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.befitter.io/" />
-        <meta property="og:image" content="https://befitter.io/item.png" />
+        <meta property="og:image" content="https://befitter.io/images/item.png" />
         <meta property="og:description" content="In Befitter, your steps are worth more than you think -- exercising and moving outdoors
   can now earn anyone tokens anytime, anywhere. We believe this simple design can nudge millions into healthier
   lifestyles and bring them to the Web3 world." />
@@ -77,7 +77,7 @@ const Home: NextPage = () => {
         <meta property="twitter:description" content="In Befitter, your steps are worth more than you think -- exercising and moving outdoors
   can now earn anyone tokens anytime, anywhere. We believe this simple design can nudge millions into healthier
   lifestyles and bring them to the Web3 world." />
-        <meta property="twitter:image" content="https://befitter.io/item.png" />
+        <meta property="twitter:image" content="https://befitter.io/images/item.png" />
         <link rel="icon" href="/favicon.png" />
 
         <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@100;200;300;400;500&display=swap" rel="stylesheet" />
@@ -85,87 +85,90 @@ const Home: NextPage = () => {
       <div className='mobile'>
         <h1>Phone devices are not supported yet</h1>
       </div>
-      <div className={styles.wrapperContent} >
-        <div style={{ transition: ".7s all cubic-bezier(.88,-0.68,.17,1.48)", transform: `translateY(-${indexActive * 20}%)` }}>
+      <div className={styles.main}>
 
-          <div className={styles.content}>
+        <div className={styles.wrapperContent} >
+          <div style={{ transition: ".7s all cubic-bezier(.88,-0.68,.17,1.48)", transform: `translateY(-${indexActive * 20}%)` }}>
 
-            <h1>Walk2Earn</h1>
-            <p>Starting your day with a short walk can
-              offer a number of health benefits & tokens.</p>
-          </div>
-          <div className={styles.content}>
+            <div className={styles.content}>
 
-            <h1>Run2Earn</h1>
-            <p>Exercising with a friend is a great way to keep
-              you motivated. Let&apos;s jog and run and earn tokens.</p>
-          </div>
-          <div className={styles.content}>
-            <h1>Cycle2Earn</h1>
-            <p>Regular cycling provides numerous health benefits
-              as your heart, blood vessels and lungs all get a workout.</p>
-          </div>
-          <div className={styles.content}>
-            <h1>Draw2Earn</h1>
-            <p>Move and draw amazing artworks on the map.
-              Then you may sell it on the market to get tokens.</p>
-          </div>
-          <div className={styles.content}>
-            <h1>Sleep2Earn</h1>
-            <p>End your daily routine by a deep sleep.
-              We pay you to sleep scientifically.</p>
+              <h1>Walk2Earn</h1>
+              <p>Starting your day with a short walk can
+                offer a number of health benefits & tokens.</p>
+            </div>
+            <div className={styles.content}>
+
+              <h1>Run2Earn</h1>
+              <p>Exercising with a friend is a great way to keep
+                you motivated. Let&apos;s jog and run and earn tokens.</p>
+            </div>
+            <div className={styles.content}>
+              <h1>Cycle2Earn</h1>
+              <p>Regular cycling provides numerous health benefits
+                as your heart, blood vessels and lungs all get a workout.</p>
+            </div>
+            <div className={styles.content}>
+              <h1>Draw2Earn</h1>
+              <p>Move and draw amazing artworks on the map.
+                Then you may sell it on the market to get tokens.</p>
+            </div>
+            <div className={styles.content}>
+              <h1>Sleep2Earn</h1>
+              <p>End your daily routine by a deep sleep.
+                We pay you to sleep scientifically.</p>
+            </div>
           </div>
         </div>
-      </div>
-      <img src="/logomain.png" className={styles.logo} alt="" />
-      <button className={styles.button} onClick={() => {
-        toast('COMING SOON!', {
-          position: "top-center",
-          autoClose: 5000,
-          hideProgressBar: true,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
+        <img src="/images/logomain.png" className={styles.logo} alt="" />
+        <button className={styles.button} onClick={() => {
+          toast('COMING SOON!', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
 
-        });
-      }}>PITCH DECK</button>
-      <Swiper
-        autoplay={{
-          "delay": dataSlide[indexActive].type === typeSlide.img ? 3000 : 6000,
-          disableOnInteraction: false,
-        }}
-        effect={"fade"}
-        pagination={pagination}
-        modules={[Pagination, Autoplay, EffectFade]}
-        onSlideChange={(e) => {
-          dataSlide[e.previousIndex].ref?.current?.pause();
-          dataSlide[e.activeIndex].ref?.current?.play();
-          setTimeout(() => { dataSlide[e.previousIndex].ref?.current?.load(); }, 500)
-          // setDurationActive(dataSlide[e.activeIndex].ref?.current?.duration * 1000 || 3000)
-          setIndexActive(e.activeIndex)
-        }}
-        className="mySwiper"
-      >
-        {
-          dataSlide.map((slide, index) => slide.type === typeSlide.video ? <SwiperSlide >
-            <video
-              id={`video${index}`}
-              ref={slide.ref}
-              autoPlay={index === 0}
-              loop
-              muted
-              playsInline
-              className='absolute'
-            >
-              <source src={slide.link} type='video/mp4; codecs="hvc1"' />
-              <source src={slide.link} type="video/webm" />
-            </video>
-          </SwiperSlide> : <SwiperSlide >
-            <img src={slide.link} className='absolute' alt="" />
-          </SwiperSlide>)
-        }
-      </Swiper>
+          });
+        }}>PITCH DECK</button>
+        <Swiper
+          autoplay={{
+            "delay": dataSlide[indexActive].type === typeSlide.img ? 3000 : 6000,
+            disableOnInteraction: false,
+          }}
+          effect={"fade"}
+          pagination={pagination}
+          modules={[Pagination, Autoplay, EffectFade]}
+          onSlideChange={(e) => {
+            dataSlide[e.previousIndex].ref?.current?.pause();
+            dataSlide[e.activeIndex].ref?.current?.play();
+            setTimeout(() => { dataSlide[e.previousIndex].ref?.current?.load(); }, 500)
+            // setDurationActive(dataSlide[e.activeIndex].ref?.current?.duration * 1000 || 3000)
+            setIndexActive(e.activeIndex)
+          }}
+          className="mySwiper"
+        >
+          {
+            dataSlide.map((slide, index) => slide.type === typeSlide.video ? <SwiperSlide key={index}>
+              <video
+                id={`video${index}`}
+                ref={slide.ref}
+                autoPlay={index === 0}
+                loop
+                muted
+                playsInline
+                className='absolute'
+              >
+                <source src={slide.link} type='video/mp4; codecs="hvc1"' />
+                <source src={slide.link} type="video/webm" />
+              </video>
+            </SwiperSlide> : <SwiperSlide key={index}>
+              <img src={slide.link} className='absolute' alt="" />
+            </SwiperSlide>)
+          }
+        </Swiper>
+      </div>
     </div>
   )
 }
