111fc6cd866b6853b6ccf2c998dd1c74a852ad69
Nam bv <nambv@versehub.io>
2022-07-06 23:51:46 +0700

    update

--- next.config.js
+++ next.config.js
@@ -21,6 +21,11 @@ module.exports = {
 
 		return config;
 	},
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
 	async redirects() {
 		return [
 			{
