b50373c4d001af9780751291f412b29a1e1a5376
hoanganh <anhnh@versehub.io>
2022-07-11 15:18:32 +0700

    add business page

---
+++ pages/business.tsx
@@ -0,0 +1,11 @@
+import HomeLayoutNew from "../components/layouts/HomeLayoutNew";
+
+const Business = () => {
+  return (
+    <HomeLayoutNew sxProps={{ background: '#111021' }} headerLandingPage={true}>
+
+    </HomeLayoutNew>
+  )
+}
+
+export default Business;
