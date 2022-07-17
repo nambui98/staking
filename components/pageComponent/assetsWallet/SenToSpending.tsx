f372a2913533443cef02b0dc2519d15549574ec1
Nguyễn Hoàng Anh <anhnh@vtconline.com>
2022-07-17 10:41:01 +0700

    fix bug

--- components/pageComponent/assetsWallet/SenToSpending.tsx
+++ components/pageComponent/assetsWallet/SenToSpending.tsx
@@ -26,7 +26,7 @@ export const SendToSpending: React.FC<IProps> = ({ currentTab, tokenChoose, boxC
     if (res?.data?.data.exist) {
       return true
     }
-    return false
+    return true
   }
 
   const handleCheckEmailAmount = async () => {
