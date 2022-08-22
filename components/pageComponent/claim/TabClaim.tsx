55785a28da20ea055fa6992e955857be1636414a
hoanganh <anhnh@versehub.io>
2022-08-22 16:13:46 +0700

    update contract

--- components/pageComponent/claim/TabClaim.tsx
+++ components/pageComponent/claim/TabClaim.tsx
@@ -39,8 +39,7 @@ type row = {
 export const TabClaim = () => {
   const router = useRouter();
   const isMobile = useMediaQuery('(max-width: 767px)');
-  const {setWalletAccount, ethersSigner, ethersProvider, updateBnbBalance, chainIdIsSupported, provider } = useWalletContext();
-  const walletAccount = '0xa478f79f010935d514c46d3e932a1331433fe8c4'
+  const {walletAccount, setWalletAccount, ethersSigner, ethersProvider, updateBnbBalance, chainIdIsSupported, provider } = useWalletContext();
   const [currentTab, setCurrentTab] = useState<string>('box');
   const [selecItem, setSelectItem] = useState<{ title: string, value: string }[]>([]);
   const [roundSelected, setRoundSelected] = useState<string>('');
