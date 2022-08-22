28b6ed67f8caaf0dc52bbc6c7ab7b5effa51a5ed
nguyenphuctuan0907 <anhjack1102@gmail.com>
2022-08-22 14:51:23 +0700

    close locked

--- constants/staking.ts
+++ constants/staking.ts
@@ -1,4 +1,4 @@
-import moment from "moment";
+import moment from 'moment';
 
 export const STAKING_ICON = {
 	search: 'assets/icons/search-normal.svg',
@@ -22,57 +22,60 @@ export const ROAD_MAP = {
 			image: STAKING_IMAGE.ftpass,
 			title: 'Fitter Pass Drops - Flexible',
 			status: 3,
-			textStatus: 'Closed'
+			textStatus: 'Closed',
 		},
 		{
 			image: STAKING_IMAGE.ftpass,
 			title: 'Fitter Pass Drops - Locked',
-			status: 2,
-			textStatus: 'LIVE'
+			status: 3,
+			textStatus: 'Closed',
 		},
 		{
 			image: STAKING_IMAGE.smartPhone,
 			title: 'Shared Pool - Yield Farming',
 			status: 1,
 			opacity: 0.2,
-			textStatus: 'Upcoming'
+			textStatus: 'Upcoming',
 		},
 		{
 			image: STAKING_IMAGE.box,
 			title: 'Fixed rate - Yield Farming',
 			status: 1,
-			textStatus: 'Upcoming'
+			textStatus: 'Upcoming',
 		},
 		{
 			image: STAKING_IMAGE.box,
 			title: 'LP Farming',
 			status: 1,
-			textStatus: 'Upcoming'
-
+			textStatus: 'Upcoming',
 		},
 		{
 			image: STAKING_IMAGE.box,
 			title: 'Profit Sharing',
 			status: 1,
-			textStatus: 'Upcoming'
-
-		}
-	]
-}
+			textStatus: 'Upcoming',
+		},
+	],
+};
 export const ROAD_MAP_AUCTION = {
 	items: [
 		{
 			image: STAKING_IMAGE.box,
 			title: 'Box Auction',
 			status: moment('2022-08-19T09:00:00.000Z') >= moment(Date.now()) ? 2 : 3,
-			textStatus: moment('2022-08-19T09:00:00.000Z') >= moment(Date.now()) ? 'LIVE' : 'Closed'
+			textStatus:
+				moment('2022-08-19T09:00:00.000Z') >= moment(Date.now())
+					? 'LIVE'
+					: 'Closed',
 		},
 		{
 			image: STAKING_IMAGE.roadmapBurnHee,
 			title: 'HEE Exchange - Win Fitter Pass',
 			status: moment('2022-08-19T10:00:00.000Z') >= moment(Date.now()) ? 2 : 3,
-			textStatus: moment('2022-08-19T10:00:00.000Z') >= moment(Date.now()) ? 'LIVE' : 'Closed'
+			textStatus:
+				moment('2022-08-19T10:00:00.000Z') >= moment(Date.now())
+					? 'LIVE'
+					: 'Closed',
 		},
-
-	]
-}
+	],
+};
--- containers/staking/components/stakingLocked/LockedStakeProcess.tsx
+++ containers/staking/components/stakingLocked/LockedStakeProcess.tsx
@@ -10,7 +10,11 @@ import { formatMoney } from '../../../../libs/utils/utils';
 import { TEXT_STYLE } from '../../../../styles/common/textStyles';
 import ArrowUp from './icons/arrow_up.svg';
 import ArrowDown from './icons/arrow_down.svg';
-import { approveStakingLocked, configStakingLocked, stakeLocked } from '../../../../libs/stakingLocked';
+import {
+	approveStakingLocked,
+	configStakingLocked,
+	stakeLocked,
+} from '../../../../libs/stakingLocked';
 
 type Props = {
 	setStateContent: Function;
@@ -57,32 +61,35 @@ export const LockedStakeProcess = ({
 		useWalletContext();
 
 	const getMinimalStakingTime = () => {
-
-		let maxP = Math.ceil(configStakingLocked.maxEarningPass / configStakingLocked.valueEstimated);
+		let maxP = Math.ceil(
+			configStakingLocked.maxEarningPass / configStakingLocked.valueEstimated
+		);
 		let ret = Math.ceil(maxP / blockNum);
 		return Math.max(ret, configStakingLocked.minLockedTime);
-	}
+	};
 	const handleApprove = async () => {
 		// let estimateFPRes = Math.floor(value * parseInt(day) * configStakingLocked.valueEstimated);
 		setIsLoading(true);
 		try {
 			const day = getMinimalStakingTime();
-			setLockDurationSuggest(0)
+			setLockDurationSuggest(0);
 			setLockDurationError(false);
 			if (day >= lockDuration) {
-				const res = await approveStakingLocked(tokenRequired.toString(), ethersSigner);
-				setIsLoading(false)
+				const res = await approveStakingLocked(
+					tokenRequired.toString(),
+					ethersSigner
+				);
+				setIsLoading(false);
 				setStatus(res.status);
 				if (res?.status) {
-					setRefresh(!refresh)
+					setRefresh(!refresh);
 				}
 			} else {
-				setIsLoading(false)
+				setIsLoading(false);
 				setLockDurationError(true);
 				setLockDuration(day);
 				setLockDurationSuggest(day);
 			}
-
 		} catch (error: any) {
 			setStatus(0);
 			const message = error.reason || 'Something went wrong, please try again';
@@ -99,10 +106,14 @@ export const LockedStakeProcess = ({
 	const handleStake = async () => {
 		setIsLoading(true);
 		try {
-			const res = await stakeLocked(blockNum.toString(), lockDuration.toString(), ethersSigner);
-			setIsLoading(false)
+			const res = await stakeLocked(
+				blockNum.toString(),
+				lockDuration.toString(),
+				ethersSigner
+			);
+			setIsLoading(false);
 			if (res?.status) {
-				setRefresh(!refresh)
+				setRefresh(!refresh);
 				handleClickSuccess({
 					titleSuccess: 'Staked successfully!',
 					functionSuccess: () => {
@@ -134,55 +145,76 @@ export const LockedStakeProcess = ({
 	};
 
 	const handleClickIncrementDay = () => {
-		setLockDurationError(false)
+		setLockDurationError(false);
 		setLockDuration((preV) => (preV = preV > 29 ? 30 : (preV += 1)));
 	};
 
 	const handleClickDecrementDay = () => {
-		setLockDurationError(false)
+		setLockDurationError(false);
 		setLockDuration((preV) => (preV = preV < 1 ? 0 : (preV -= 1)));
 	};
 	const tokenRequired = blockNum * configStakingLocked.valueTokenBlock;
-	const estimatedReceivableRewards = lockDuration > 2 ? Math.floor(blockNum * lockDuration * configStakingLocked.valueEstimated) : 0;
+	const estimatedReceivableRewards =
+		lockDuration > 2
+			? Math.floor(blockNum * lockDuration * configStakingLocked.valueEstimated)
+			: 0;
 	return (
-		<Box >
+		<Box>
 			<Item sx={{ mt: '16px!important' }}>
 				<TitleItem>your current balance</TitleItem>
 				<ValueItem>{formatMoney(balanceFiu)} FIU</ValueItem>
 			</Item>
 			<Item sx={{ mt: '16px!important' }}>
-				<TitleItem sx={{ mr: "auto" }}>Choose your block(s)</TitleItem>
-				<ValueItem sx={{ pointerEvents: "none" }}>
+				<TitleItem sx={{ mr: 'auto' }}>Choose your block(s)</TitleItem>
+				<ValueItem sx={{ pointerEvents: 'none' }}>
 					<Search value={blockNum} placeholder="" />
 				</ValueItem>
 				<Box sx={{ marginLeft: '8px' }}>
-					<ButtonCustom variant="text" disabled={blockNum === 12 || status === 1} onClick={handleClickIncrement}>
+					<ButtonCustom
+						variant="text"
+						disabled={blockNum === 12 || status === 1}
+						onClick={handleClickIncrement}
+					>
 						<ArrowUp />
 					</ButtonCustom>
-					<ButtonCustom variant="text" disabled={blockNum === 0 || status === 1} sx={{ mt: "2px" }} onClick={handleClickDecrement}>
+					<ButtonCustom
+						variant="text"
+						disabled={blockNum === 0 || status === 1}
+						sx={{ mt: '2px' }}
+						onClick={handleClickDecrement}
+					>
 						<ArrowDown />
 					</ButtonCustom>
-
 				</Box>
-			</Item >
-			<Item sx={{ mt: "8px !important" }}>
-				<TitleItem ></TitleItem>
-				<ValueItem sx={{ display: "flex", gap: "8px" }}>
-					<ButtonPercent disabled={status === 1} onClick={() => { setBlockNum(1) }} variant="text">
+			</Item>
+			<Item sx={{ mt: '8px !important' }}>
+				<TitleItem></TitleItem>
+				<ValueItem sx={{ display: 'flex', gap: '8px' }}>
+					<ButtonPercent
+						disabled={status === 1}
+						onClick={() => {
+							setBlockNum(1);
+						}}
+						variant="text"
+					>
 						Min (1)
 					</ButtonPercent>
-					<ButtonPercent disabled={status === 1} onClick={() => { setBlockNum(12) }} variant="text">
+					<ButtonPercent
+						disabled={status === 1}
+						onClick={() => {
+							setBlockNum(12);
+						}}
+						variant="text"
+					>
 						Max (12)
 					</ButtonPercent>
-
 				</ValueItem>
 			</Item>
 			<Item sx={{ mt: '16px!important' }}>
 				<TitleItem>Token required</TitleItem>
 				<ValueItem>{formatMoney(tokenRequired.toString())} FIU</ValueItem>
 			</Item>
-			{
-				tokenRequired > parseFloat(balanceFiu) &&
+			{tokenRequired > parseFloat(balanceFiu) && (
 				<Box
 					sx={{
 						textAlign: 'end',
@@ -195,53 +227,67 @@ export const LockedStakeProcess = ({
 				>
 					You don’t have enough token
 				</Box>
-			}
+			)}
 			<Item sx={{ display: 'flex', mt: '16px !important' }}>
-				<TitleItem sx={{ mr: "auto" }}>Lock duration (days)</TitleItem>
-				<ValueItem sx={{ pointerEvents: "none" }}>
+				<TitleItem sx={{ mr: 'auto' }}>Lock duration (days)</TitleItem>
+				<ValueItem sx={{ pointerEvents: 'none' }}>
 					<Search value={lockDuration} placeholder="" />
 				</ValueItem>
 
 				<Box sx={{ marginLeft: '8px' }}>
-					<ButtonCustom variant="text" disabled={lockDuration === 30 || status === 1} onClick={handleClickIncrementDay}>
+					<ButtonCustom
+						variant="text"
+						disabled={lockDuration === 30 || status === 1}
+						onClick={handleClickIncrementDay}
+					>
 						<ArrowUp />
 					</ButtonCustom>
-					<ButtonCustom variant="text" disabled={lockDuration < 4 || status === 1} sx={{ mt: "2px" }} onClick={handleClickDecrementDay}>
+					<ButtonCustom
+						variant="text"
+						disabled={lockDuration < 4 || status === 1}
+						sx={{ mt: '2px' }}
+						onClick={handleClickDecrementDay}
+					>
 						<ArrowDown />
 					</ButtonCustom>
 				</Box>
 			</Item>
-			<Item sx={{ mt: "8px !important" }}>
-				<TitleItem ></TitleItem>
-				<ValueItem sx={{ display: "flex", gap: "8px" }}>
-					<ButtonPercent onClick={() => {
-						setLockDurationError(false)
-						setLockDuration(3)
-					}} disabled={status === 1} variant="text">
+			<Item sx={{ mt: '8px !important' }}>
+				<TitleItem></TitleItem>
+				<ValueItem sx={{ display: 'flex', gap: '8px' }}>
+					<ButtonPercent
+						onClick={() => {
+							setLockDurationError(false);
+							setLockDuration(3);
+						}}
+						disabled={status === 1}
+						variant="text"
+					>
 						Min (3)
 					</ButtonPercent>
-					<ButtonPercent onClick={() => {
-						setLockDurationError(false)
-						setLockDuration(30)
-					}} disabled={status === 1} variant="text">
+					<ButtonPercent
+						onClick={() => {
+							setLockDurationError(false);
+							setLockDuration(30);
+						}}
+						disabled={status === 1}
+						variant="text"
+					>
 						Max (30)
 					</ButtonPercent>
-
 				</ValueItem>
 			</Item>
-			{
-				messageError && (
-					<Item sx={{ mt: '8px !important' }}>
-						<TitleItem></TitleItem>
-						<ValueItem>
-							{' '}
-							<Typography sx={{ ...TEXT_STYLE(12, 500, '#FF6F61') }}>
-								{messageError}
-							</Typography>
-						</ValueItem>
-					</Item>
-				)
-			}
+			{messageError && (
+				<Item sx={{ mt: '8px !important' }}>
+					<TitleItem></TitleItem>
+					<ValueItem>
+						{' '}
+						<Typography sx={{ ...TEXT_STYLE(12, 500, '#FF6F61') }}>
+							{messageError}
+						</Typography>
+					</ValueItem>
+				</Item>
+			)}
 
 			<Item sx={{ mt: '18px !important' }}>
 				<TitleItem>
@@ -258,15 +304,12 @@ export const LockedStakeProcess = ({
 						Maximum: 31 Fitter Passes
 					</Box>
 				</TitleItem>
-				<ValueItem
-					sx={{ display: 'flex', alignItems: 'center' }}
-				>
+				<ValueItem sx={{ display: 'flex', alignItems: 'center' }}>
 					<span>{Math.min(estimatedReceivableRewards, 31)}</span>
 					<img src="images/ffPase.svg" alt="" />
 				</ValueItem>
 			</Item>
-			{
-				lockDurationError === true &&
+			{lockDurationError === true && (
 				<Item sx={{ mt: '8px !important' }}>
 					<TitleItem
 						sx={{
@@ -276,56 +319,66 @@ export const LockedStakeProcess = ({
 							height: '54px',
 						}}
 					>
-						You have reached maximum reward!<br />
-						Suggest: With {lockDurationSuggest.toString()} days of lock duration, you still maximize your rewards without locking your token for too long.
+						You have reached maximum reward!
+						<br />
+						Suggest: With {lockDurationSuggest.toString()} days of lock
+						duration, you still maximize your rewards without locking your token
+						for too long.
 					</TitleItem>
 				</Item>
-			}
+			)}
 			<Box
 				mt="auto !important"
 				width={'100%'}
 				sx={{ paddingTop: '16px', borderTop: '1px solid #E9EAEF' }}
 			>
-				{
-					status === 1 ?
-						<MarketplaceButton
-							disabled={!blockNum || messageError || tokenRequired > parseFloat(balanceFiu) ? true : false}
-							customStyle={{ width: '100%' }}
-							title={'Stake'}
-							handleOnClick={handleStake}
-						/>
-						: <MarketplaceButton
-							disabled={!blockNum || messageError || tokenRequired > parseFloat(balanceFiu) || lockDuration < 3 ? true : false}
-							customStyle={{ width: '100%' }}
-							title={'Approve'}
-							handleOnClick={handleApprove}
-						/>
-				}
-
+				{status === 1 ? (
+					<MarketplaceButton
+						// 	disabled={
+						// 		!blockNum ||
+						// 		messageError ||
+						// 		tokenRequired > parseFloat(balanceFiu)
+						// 			? true
+						// 			: false
+						// 	}
+						disabled={true}
+						customStyle={{ width: '100%' }}
+						title={'Stake'}
+						handleOnClick={handleStake}
+					/>
+				) : (
+					<MarketplaceButton
+						// disabled={!blockNum || messageError || tokenRequired > parseFloat(balanceFiu) || lockDuration < 3 ? true : false}
+						disabled={true}
+						customStyle={{ width: '100%' }}
+						title={'Approve'}
+						handleOnClick={handleApprove}
+					/>
+				)}
 			</Box>
-		</Box >
+		</Box>
 	);
 };
 const ButtonPercent = styled(Button)({
-	color: "#55C8FC",
-	background: " #D9F2FD",
-	borderRadius: "4px",
-	padding: "5px 5px",
-	minWidth: "35px",
-	height: "26px",
-	textTransform: "none",
-	fontSize: "12px",
-	fontWeight: "500",
-	"&:hover": {
-		background: " #d0edfa !important",
+	color: '#55C8FC',
+	background: ' #D9F2FD',
+	borderRadius: '4px',
+	padding: '5px 5px',
+	minWidth: '35px',
+	height: '26px',
+	textTransform: 'none',
+	fontSize: '12px',
+	fontWeight: '500',
+	'&:hover': {
+		background: ' #d0edfa !important',
 	},
 	'&:disabled': {
 		background: '#E9EAEF',
 		'&>svg': {
-			stroke: "#A7ACB8"
-		}
-	}
-})
+			stroke: '#A7ACB8',
+		},
+	},
+});
 const ButtonCustom = styled(Button)({
 	color: '#55C8FC',
 	background: ' #D9F2FD',
@@ -335,12 +388,12 @@ const ButtonCustom = styled(Button)({
 	height: '16px',
 	border: 'none',
 	textTransform: 'none',
-	display: "flex",
+	display: 'flex',
 	alignItems: 'center',
 	justifyContent: 'center',
 	'&>svg': {
-		pointerEvents: "none",
-		stroke: "#55C8FC",
+		pointerEvents: 'none',
+		stroke: '#55C8FC',
 	},
 	'&:hover': {
 		background: ' #d0edfa !important',
@@ -348,9 +401,9 @@ const ButtonCustom = styled(Button)({
 	'&:disabled': {
 		background: '#E9EAEF',
 		'&>svg': {
-			stroke: "#A7ACB8"
-		}
-	}
+			stroke: '#A7ACB8',
+		},
+	},
 });
 const Search = styled(TextField)({
 	'& .MuiOutlinedInput-root': {
